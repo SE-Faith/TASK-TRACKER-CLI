459450ed742e38691bac048844fc4c8d86c6b24a
SE-Faith <omobolanleomolafe@gmail.com>
2026-06-07 06:33:40 +0100

    feat: update tasks

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import { addTask } from './commands/add.js';
+import { listTasks, listTasksByStatus } from './commands/list.js';
+import { updateTask } from './commands/update.js';
 
 const args = process.argv.slice(2);
 
 if (args[0] === 'add') {
     addTask(args[1]);
+}
+
+if (args[0] === 'list') {
+    listTasks();
+}
+
+if (args[0] === 'list-by') {
+    listTasksByStatus(args[1]);
+}
+
+if (args[0] === 'update') {
+    updateTask(args[1], args.slice(2).join(' '));
 }
